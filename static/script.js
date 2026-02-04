318ec005e7360bb35205dba7e75d16a57edfce49
godwinlevel139-cpu <godwinlevel139@gmail.com>
2026-02-04 03:19:30 +0100

    Delete static directory

--- static/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector('form');
-    if(form) {
-        form.addEventListener('submit', function() {
-            alert("Thank you for registering! Your submission is being processed.");
-        });
-    }
-});
